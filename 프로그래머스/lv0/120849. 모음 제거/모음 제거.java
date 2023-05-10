c52addd851e8ae738a336cfd07d14e647a69bddb
jongmin-p <119127039+jongmin-p@users.noreply.github.com>
2023-05-11 00:59:10 +0900

    Update 모음 제거.java

--- 프로그래머스/lv0/120849. 모음 제거/모음 제거.java
+++ 프로그래머스/lv0/120849. 모음 제거/모음 제거.java
@@ -4,3 +4,13 @@ class Solution {
         return my_string.replaceAll("[aeiou]", "");
     }
 }
+
+/*
+        String str = "aabbcdd";
+
+        String result1 = str.replace("bcd", "*");       // 특정 문자열만 변환 가능
+        String result2 = str.replaceAll("[bcd]", "*");  // 정규식 이용 가능
+
+        System.out.println("replace()    --- result -> " + result1);
+        System.out.println("replaceAll() --- result -> " + result2);
+*/
