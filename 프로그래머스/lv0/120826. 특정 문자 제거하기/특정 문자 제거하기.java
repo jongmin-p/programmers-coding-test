c3b4750c7b33925d2e43252321810fd9aa97ae76
jongmin-p <119127039+jongmin-p@users.noreply.github.com>
2023-05-11 00:49:32 +0900

    Update 특정 문자 제거하기.java

--- 프로그래머스/lv0/120826. 특정 문자 제거하기/특정 문자 제거하기.java
+++ 프로그래머스/lv0/120826. 특정 문자 제거하기/특정 문자 제거하기.java
@@ -1,8 +1,6 @@
 class Solution {
     public String solution(String my_string, String letter) {
-        String answer = my_string.replace(letter, "");
-        System.out.println(answer);
 
-        return answer;
+        return my_string.replace(letter, "");
     }
 }
