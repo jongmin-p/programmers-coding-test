47bcf35c96aea9c42afca470fd1c3e1ba9bee48a
jongmin-p <119127039+jongmin-p@users.noreply.github.com>
2023-05-11 00:50:04 +0900

    Update 문자 반복 출력하기.java

--- 프로그래머스/lv0/120825. 문자 반복 출력하기/문자 반복 출력하기.java
+++ 프로그래머스/lv0/120825. 문자 반복 출력하기/문자 반복 출력하기.java
@@ -1,13 +1,14 @@
 class Solution {
     public String solution(String my_string, int n) {
         String answer = "";
-        String[] each = my_string.split("");
+        String[] eachLetter = my_string.split("");            // 문자열 쪼개기
 
+        // 문자열의 길이 - 1 만큼 반복
         for(int i = 0; i <= my_string.length() - 1; i++) {
-            answer += each[i].repeat(n);
+            // 쪼갠 문자열의 각 단어들(eachLetter[i]) 을 n 번씩 반복해서 이어 붙이기
+            answer += eachLetter[i].repeat(n);
         }
 
-        System.out.println(answer);
         return answer;
     }
 }
