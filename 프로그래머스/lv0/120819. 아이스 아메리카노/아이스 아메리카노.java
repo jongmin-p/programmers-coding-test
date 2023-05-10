436e82ea928e3d27d4b4d6e55fd99b0d7ea4a375
jongmin-p <119127039+jongmin-p@users.noreply.github.com>
2023-05-10 14:17:40 +0900

    [level 0] Title: 아이스 아메리카노, Time: 0.01 ms, Memory: 76.8 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120819. 아이스 아메리카노/아이스 아메리카노.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int[] solution(int money) {
+        int[] answer = new int[2];
+        int coffee = 5500;
+        int change = 0;
+        
+        answer[0] = money / coffee;
+        answer[1] = money % coffee;
+
+        
+        return answer;
+    }
+}
