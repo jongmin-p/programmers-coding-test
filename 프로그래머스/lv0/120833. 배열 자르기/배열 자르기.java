25e0c1519897420eea295a5d019415058fc30e79
jongmin-p <119127039+jongmin-p@users.noreply.github.com>
2023-05-12 12:21:56 +0900

    Update 배열 자르기.java

--- 프로그래머스/lv0/120833. 배열 자르기/배열 자르기.java
+++ 프로그래머스/lv0/120833. 배열 자르기/배열 자르기.java
@@ -4,6 +4,7 @@ class Solution {
     public int[] solution(int[] numbers, int num1, int num2) {
         int[] answer = {};
 
+        // num1 번째 인덱스부터 ~ num2 번째 인덱스까지 복사 (마지막은 복사 안 되기에 num2 + 1)
         answer = Arrays.copyOfRange(numbers, num1, num2 + 1);
         return answer;
     }
