695647902f79acfa69c491a8528cb8ced73a4b8c
jongmin-p <119127039+jongmin-p@users.noreply.github.com>
2023-05-12 18:01:51 +0900

    Update 숨어있는 숫자의 덧셈 （1）.java

--- 프로그래머스/lv0/120851. 숨어있는 숫자의 덧셈 （1）/숨어있는 숫자의 덧셈 （1）.java
+++ 프로그래머스/lv0/120851. 숨어있는 숫자의 덧셈 （1）/숨어있는 숫자의 덧셈 （1）.java
@@ -3,7 +3,8 @@ class Solution {
         int answer = 0;
         String[] str = my_string.split("");
 
-        // 만약 문자열 str 각 요소 중에서, 정수 0 ~ 9 사이의 숫자가 나오면, 해당 문자열을 정수로 바꾼 후 answer 에 더해라
+        // 만약 문자열 str 각 요소 중에서, 정수 0 ~ 9 사이의 숫자가 나오면,
+        // 해당 문자열을 정수로 바꾼 후 answer 에 더해라
         for (String s : str) {
             if (s.matches("[0-9]")) {
                 answer += Integer.valueOf(s);
