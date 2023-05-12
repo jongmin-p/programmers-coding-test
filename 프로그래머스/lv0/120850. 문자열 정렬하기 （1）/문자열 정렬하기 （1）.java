b8e1e358abc9d5a4f690750f9c1ed65777250152
jongmin-p <119127039+jongmin-p@users.noreply.github.com>
2023-05-12 18:10:59 +0900

    [level 0] Title: 문자열 정렬하기 (1), Time: 1.23 ms, Memory: 73.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120850. 문자열 정렬하기 （1）/문자열 정렬하기 （1）.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(String my_string) {
+        ArrayList<Integer> list = new ArrayList<>();
+        String[] str = my_string.split("");
+        int[] answer = {};
+
+
+        for(String s : str) {
+            if (s.matches("[0-9]")) {
+                list.add(Integer.valueOf(s));
+            }
+        }
+
+        // ArrayList 를 일반 정수 배열로 변환
+        answer = new int[list.size()];
+        
+        for (int i = 0 ; i < list.size() ; i++) {
+            answer[i] = list.get(i).intValue();
+        }
+
+        // 배열 오름차순 정렬
+        Arrays.sort(answer);
+        
+        return answer;
+    }
+}
